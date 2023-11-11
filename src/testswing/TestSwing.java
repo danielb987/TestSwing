14a43301afef4977633ab2b92ce97765b6d67573
Daniel <db123@bergqvist.se>
2023-11-11 08:40:34 +0100

    First commit

---
+++ src/testswing/TestSwing.java
@@ -0,0 +1,16 @@
+package testswing;
+
+/**
+ * Test Swing
+ * @author Daniel Bergqvist
+ */
+public class TestSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
